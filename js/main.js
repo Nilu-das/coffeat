b52e024e73acd05bddb8e1e32c6e560e09ddcb0f
Nilu-das <niluranidas08223@gmail.com>
2020-06-17 01:45:33 +0600

    coffeat project complet with responsive

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
 $(document).ready(function() {
-    $('.  img-slide').slick({
-        dots: true,
-        arrows: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true
-    });
 
-    // $('.img-slide').slick({
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     arrows: false,
-    //     fade: true,
-    //     asNavFor: '.text-slide'
-    // });
-    // $('.text-slide').slick({
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     asNavFor: '.img-slide',
-    //     dots: true,
-    //     arrows: false,
-    //     centerMode: true,
-    //     focusOnSelect: true
-    // });
 })
